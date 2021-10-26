273631cbcf1bb15f8f0bcd7a7968c8f50f281826
langpengzhen <langpengzhen@kuaishou.com>
2021-10-26 21:14:30 +0800

    ks报错

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,40 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import {ref} from 'vue'
 import Logicflow from './process/index.vue';
+const nodeTemplate = ref()
+const nodeTemplateOptions = [
+    { label: '结束模板', value: 'over' },
+    { label: '处理中模板', value: 'processing' },
+    { label: '待接单模板', value: 'pending' },
+    { label: '系统持单模板', value: 'system' }
+];
 </script>
 
 <template>
+    <ks-select
+        v-model="nodeTemplate"
+        placeholder="请选择"
+    >
+        <ks-option
+            v-for="item in nodeTemplateOptions"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+        >
+        </ks-option>
+    </ks-select>
+    <!-- <el-select
+        v-model="nodeTemplate"
+        placeholder="请选择"
+    >
+        <el-option
+            v-for="item in nodeTemplateOptions"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+        >
+        </el-option>
+    </el-select> -->
     <Logicflow />
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import KwaiUI from '@ks/kwai-ui';
+import '@ks/kwai-ui/lib/theme-new-era/index.css';
+const app = createApp(App);
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+app.use(ElementPlus);
+app.use(KwaiUI);
+app.mount('#app');
